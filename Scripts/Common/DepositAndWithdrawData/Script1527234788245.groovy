dae1bd24449ecfdce45f1f55efe747410cdb154d
hsiang731219 <hsiang731219@gmail.com>
2018-05-28 09:50:38 +0800

    遺漏 groovy 檔案

---
+++ Scripts/Common/DepositAndWithdrawData/Script1527234788245.groovy
@@ -0,0 +1,56 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.ExcelData as ExcelData
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testdata.reader.ExcelFactory as ExcelFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import sun.awt.datatransfer.ClipboardTransferable.DataFactory as DataFactory
+
+def dAndWInfo = [ 'account' : '' ,
+	              'amount' : '' ,
+				  'depositpassword' : '',
+				  'withdrawpassword' : '',
+				  'auditAmount' : '',
+				  'noNeedToAudit' : '',
+				  'depositAudit' :'',
+				  'promotionAudit' :'',
+				  'type1' :'',
+				  'type2' :'',
+				  'type3' :'',
+				  'type4' :'',
+				  'type5' :'']
+
+ExcelData DepositAndWithdrawInfo = ExcelFactory.getExcelDataWithDefaultSheet("D:/TestData.xlsx", "DepositAndWithdrawInfo", false)
+
+dAndWInfo.account = DepositAndWithdrawInfo.getValue(1, 2)
+dAndWInfo.amount = Integer.parseInt(DepositAndWithdrawInfo.getValue(2, 2))
+dAndWInfo.depositpassword = DepositAndWithdrawInfo.getValue(3, 2)
+dAndWInfo.withdrawpassword = DepositAndWithdrawInfo.getValue(4, 2)
+dAndWInfo.auditAmount = Integer.parseInt(DepositAndWithdrawInfo.getValue(5, 2))
+dAndWInfo.noNeedToAudit = DepositAndWithdrawInfo.getValue(1, 4)
+dAndWInfo.depositAudit = DepositAndWithdrawInfo.getValue(2, 4)
+dAndWInfo.promotionAudit = DepositAndWithdrawInfo.getValue(3, 4)
+dAndWInfo.type1 = DepositAndWithdrawInfo.getValue(1, 5)
+dAndWInfo.type2 = DepositAndWithdrawInfo.getValue(2, 5)
+dAndWInfo.type3 = DepositAndWithdrawInfo.getValue(3, 5)
+dAndWInfo.type4 = DepositAndWithdrawInfo.getValue(4, 5)
+dAndWInfo.type5 = DepositAndWithdrawInfo.getValue(5, 5)
+
+
+return dAndWInfo
---
+++ Scripts/Common/TestEnvironmenInfoData/Script1527229189153.groovy
@@ -0,0 +1,51 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.ExcelData as ExcelData
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testdata.reader.ExcelFactory as ExcelFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import sun.awt.datatransfer.ClipboardTransferable.DataFactory as DataFactory
+
+def Gpk_Info = [('Gpk_admin') : '', ('Gpk_master') : '', ('Gpk_agent') : '', ('Gpk_protal') : '', ('Gpk_mobile') : '', ('Release_admin') : ''
+    , ('Release_master') : '', ('Release_agent') : '', ('Release_protal') : '', ('Release_mobile') : '']
+
+ExcelData Url_Info = ExcelFactory.getExcelDataWithDefaultSheet('D:/TestData.xlsx', 'TestEnvironmenInfo', false)
+
+Gpk_Info.Gpk_admin = Url_Info.getValue(2, 2)
+
+Gpk_Info.Gpk_master = Url_Info.getValue(3, 2)
+
+Gpk_Info.Gpk_agent = Url_Info.getValue(4, 2)
+
+Gpk_Info.Gpk_protal = Url_Info.getValue(5, 2)
+
+Gpk_Info.Gpk_mobile = Url_Info.getValue(6, 2)
+
+Gpk_Info.Release_admin = Url_Info.getValue(2, 3)
+
+Gpk_Info.Release_master = Url_Info.getValue(3, 3)
+
+Gpk_Info.Release_agent = Url_Info.getValue(4, 3)
+
+Gpk_Info.Release_protal = Url_Info.getValue(5, 3)
+
+Gpk_Info.Release_mobile = Url_Info.getValue(6, 3)
+
+return Gpk_Info
+
