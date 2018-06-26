b65d4c442f98767d77551e539c187894caded3e9
Unknown <hsiang731219@gmail.com>
2018-06-26 13:18:47 +0800

    Merger 後衝突消失
    存取款數據

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -30,6 +30,14 @@ def static "common.MasterLogin.Login"() {
     (new common.MasterLogin()).Login()
 }
 
+def static "common.MenuIntoPage.getDropdownMenu"(
+    	int x	
+     , 	int y	) {
+    (new common.MenuIntoPage()).getDropdownMenu(
+        	x
+         , 	y)
+}
+
 def static "extension.DataConversion.CurrencyToInt"(
     	String point	) {
     (new extension.DataConversion()).CurrencyToInt(
@@ -50,12 +58,4 @@ def static "extension.DataConversion.yearmonthdate"(
     	String point	) {
     (new extension.DataConversion()).yearmonthdate(
         	point)
-}
-
-def static "common.MenuIntoPage.getDropdownMenu"(
-    	int x	
-     , 	int y	) {
-    (new common.MenuIntoPage()).getDropdownMenu(
-        	x
-         , 	y)
 }
